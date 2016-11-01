6f128b4441ba4d11e4b9f6a172878c6f6d10762c
jacobemerick <jpemeric@gmail.com>
2016-11-01 14:58:27 -0700

    Adds tests around handler

---
+++ tests/unit/ErrorHandlerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AvalancheDevelopment\CrashPad;
+
+use AvalancheDevelopment\Peel\HttpError\NotFound;
+use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\StreamInterface as Stream;
+
+class ErrorHandlerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testInvokeHandlesNormalExceptionAsServerError()
+    {
+        $mockRequest = $this->createMock(Request::class);
+
+        $mockBody = $this->createMock(Stream::class);
+        $mockBody->expects($this->once())
+            ->method('write')
+            ->with(json_encode([
+                'statusCode' => 500,
+                'error' => 'Internal Server Error',
+                'message' => 'some exception',
+            ]));
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->expects($this->once())
+            ->method('withStatus')
+            ->with(500)
+            ->will($this->returnSelf());
+        $mockResponse->expects($this->once())
+            ->method('getBody')
+            ->willReturn($mockBody);
+
+        $exception = new \Exception('some exception');
+
+        $result = (new ErrorHandler)($mockRequest, $mockResponse, $exception);
+        $this->assertSame($mockResponse, $result);
+    }
+
+    public function testInvokeHandlesHttpErrorAsCustomError()
+    {
+        $mockRequest = $this->createMock(Request::class);
+
+        $mockBody = $this->createMock(Stream::class);
+        $mockBody->expects($this->once())
+            ->method('write')
+            ->with(json_encode([
+                'statusCode' => 404,
+                'error' => 'Not Found',
+                'message' => 'some not found exception',
+            ]));
+
+        $mockResponse = $this->createMock(Response::class);
+        $mockResponse->expects($this->once())
+            ->method('withStatus')
+            ->with(404)
+            ->will($this->returnSelf());
+        $mockResponse->expects($this->once())
+            ->method('getBody')
+            ->willReturn($mockBody);
+
+        $exception = new NotFound('some not found exception');
+
+        $result = (new ErrorHandler)($mockRequest, $mockResponse, $exception);
+        $this->assertSame($mockResponse, $result);
+    }
+}
